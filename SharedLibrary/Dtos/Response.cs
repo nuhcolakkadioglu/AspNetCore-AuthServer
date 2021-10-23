771cde293c9ce066f2c5aabd4e74036517fda9fc
nuhcolakkadioglu <hex-tr@hotmail.com>
2021-10-23 21:20:52 +0300

    Update

--- SharedLibrary/Dtos/Response.cs
+++ SharedLibrary/Dtos/Response.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace SharedLibrary.Dtos
@@ -12,13 +13,16 @@ namespace SharedLibrary.Dtos
         public int StatusCode { get; private set; }
         public ErrorDto Error { get; set; }
 
+        [JsonIgnore]
+        public bool IsSuccessful { get; set; }
+
         public static Response<T> Success(T data, int statusCode)
         {
-            return new Response<T> { Data = data, StatusCode = statusCode };
+            return new Response<T> { Data = data, StatusCode = statusCode, IsSuccessful = true };
         }
         public static Response<T> Success(int statusCode)
         {
-            return new Response<T> { Data = default, StatusCode = statusCode };
+            return new Response<T> { Data = default, StatusCode = statusCode,IsSuccessful =true };
         }
 
         public static Response<T> Fail(ErrorDto errorDto, int statusCode)
@@ -26,7 +30,8 @@ namespace SharedLibrary.Dtos
             return new Response<T>
             {
                 Error = errorDto,
-                StatusCode = statusCode
+                StatusCode = statusCode,
+                IsSuccessful =false
             };
         }
         public static Response<T> Fail(string errorMessage, int statusCode, bool isShow)
@@ -35,7 +40,8 @@ namespace SharedLibrary.Dtos
             return new Response<T>
             {
                 Error = errorDto,
-                StatusCode = statusCode
+                StatusCode = statusCode,
+                IsSuccessful=false
             };
         }
     }
