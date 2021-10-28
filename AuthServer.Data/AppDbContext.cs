2c71d36d072a4486ceb0a372ff2bd9f4270d1e32
nuhcolakkadioglu <hex-tr@hotmail.com>
2021-10-28 19:31:44 +0300

    dbContext Oluşturuldu

---
+++ AuthServer.Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using AuthServer.Core.Models;
+using AuthServer.Data.Configurations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Data
+{
+    public class AppDbContext : IdentityDbContext<UserApp, IdentityRole, string>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<UserRefreshToken> UserRefreshTokens { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ AuthServer.Data/Configurations/ProductConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using AuthServer.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Data.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(m => m.Id);
+            builder.Property(m=>m.Name).IsRequired().HasMaxLength(50);
+            builder.Property(m => m.Stock).IsRequired();
+            builder.Property(m => m.Price).HasColumnType("decimal(18,2)");
+            builder.Property(m => m.UserId).IsRequired();
+        }
+    }
+}
